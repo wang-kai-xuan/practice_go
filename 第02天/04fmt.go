56b8758312f4835f8d12346c7277e4bde4bf3c80
wangkaixuan <2654189525@qq.com>
2019-02-05 14:23:03 +0800

    EN: 练习了数字的不同格式的输出（地址，x进制）

---
+++ 第02天/04fmt.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main0401() {
+	a := 123
+	fmt.Printf("%b\n", a)
+	fmt.Printf("%o\n", a)
+	fmt.Printf("%x\n", a)
+	fmt.Printf("%X\n", a)
+	fmt.Printf("%d\n", a)
+}
+func main() {
+	a := "王凯旋"
+	fmt.Printf("%p\n", &a)
+}
