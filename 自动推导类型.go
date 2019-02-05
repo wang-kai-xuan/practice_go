56ea6813c23c0dd51566dbba34bcc60ef6d73520
wangkaixuan <2654189525@qq.com>
2019-02-05 12:54:28 +0800

    EN: 学习了自动推导类型的写法

---
+++ 自动推导类型.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10.0
+	b := 123.456
+	c := "王凯旋"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	a,b = b, a
+
+	fmt.Println(a)
+	fmt.Println(b)
+}
