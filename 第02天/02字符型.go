abf65ae1155b872a042d86f9f0adeb62f4d021e7
wangkaixuan <2654189525@qq.com>
2019-02-05 14:10:02 +0800

    EN: 练习了数字转字符

---
+++ 第02天/02字符型.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a byte = 'a'
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%c\n", 97)
+
+	var b byte = '0'
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+}
