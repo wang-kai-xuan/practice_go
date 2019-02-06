7ba4a8c0abbc9125c5f90b576d9404ad42b07535
wangkaixuan <2654189525@qq.com>
2019-02-06 16:04:06 +0800

    EN: 练习了动态数组（切片）的定义和访问

---
+++ 第05天-数组和切片/06切片-动态数组.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr_slice []int = []int{1, 2, 3, 4, 5}
+	fmt.Println(arr_slice)
+	arr_slice[0] = 123
+	arr_slice[2] = 456
+	fmt.Println(arr_slice)
+	arr_slice = append(arr_slice, 6)
+	fmt.Println(arr_slice)
+}
