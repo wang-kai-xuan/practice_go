6073adc872b247bb81c55f43896ed7c1853471bf
wangkaixuan <2654189525@qq.com>
2019-02-11 20:23:22 +0800

    EN: 练习了数组作为参数传递-数组是值传递

---
+++ 第06天-字符串处理函数和map结构体/01数组作为函数参数.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(arr [10]int) [10]int {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
+func main() {
+	arr := [10]int{10, 3, 2, 1, 4, 5, 6, 7, 8, 9}
+	fmt.Println(arr)
+	fmt.Println(swap(arr))
+	fmt.Println(arr)
+}
