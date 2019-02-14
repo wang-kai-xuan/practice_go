cf60384ad5bf4009f98613221404279e4e0044f1
wangkaixuan <2654189525@qq.com>
2019-02-14 23:30:40 +0800

    EN: 练习了对象方法的创建和使用

--- 第08天-面向对象上/02对象方法的创建和使用.go
+++ 第08天-面向对象上/02对象方法的创建和使用.go
@@ -4,7 +4,22 @@ import "fmt"
 
 type MyInt int
 
-func main() {
+type Student struct {
+	id   int
+	name string
+	age  int
+}
+
+func (s Student) print() {
+	fmt.Printf("学号:%d\n", s.id)
+	fmt.Printf("名字:%s\n", s.name)
+	fmt.Printf("年龄:%d\n", s.age)
+}
+func main01() {
 	var a MyInt = 2
 	fmt.Println(a)
+}
+func main() {
+	stu := Student{1, "wangkaixuan", 24}
+	stu.print()
 }
