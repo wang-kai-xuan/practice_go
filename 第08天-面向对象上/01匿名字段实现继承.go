4e3deb41743ebf6e197a4ba71c4e24ab92359a6d
wangkaixuan <2654189525@qq.com>
2019-02-14 22:45:36 +0800

    EN: 练习了使用匿名字段实现继承时，如果父类和子类有相同的成员名，默认赋值给子类

--- 第08天-面向对象上/01匿名字段实现继承.go
+++ 第08天-面向对象上/01匿名字段实现继承.go
@@ -9,12 +9,13 @@ type Person struct {
 }
 type Student struct {
 	Person
+	name  string
 	class int
 	score int
 }
 
 func main() {
-	stu := Student{Person{1, "wangkaixuan", 24}, 1, 100}
+	stu := Student{Person{1, "wangkaixuan", 24}, "wkx", 1, 100}
 	fmt.Println(stu)
 	stu.name = "chengangrong"
 	fmt.Println(stu)
