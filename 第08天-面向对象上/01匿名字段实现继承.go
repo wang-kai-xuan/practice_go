a9f081739a29d4b22418b0e9f6e3877afa67b0ee
wangkaixuan <2654189525@qq.com>
2019-02-14 22:50:04 +0800

    EN: 练习了使用匿名字段实现继承时，使用指针的情况

--- 第08天-面向对象上/01匿名字段实现继承.go
+++ 第08天-面向对象上/01匿名字段实现继承.go
@@ -8,15 +8,23 @@ type Person struct {
 	age  int
 }
 type Student struct {
-	Person
+	*Person
 	name  string
 	class int
 	score int
 }
 
 func main() {
-	stu := Student{Person{1, "wangkaixuan", 24}, "wkx", 1, 100}
+
+	stu := Student{&Person{1, "wangkaixuan", 24}, "wkx", 1, 100}
 	fmt.Println(stu)
 	stu.name = "chengangrong"
 	fmt.Println(stu)
+
+	fmt.Printf("%d\n", stu.id)
+	fmt.Printf("%s\n", stu.name)
+	fmt.Printf("%s\n", stu.Person.name)
+	fmt.Printf("%d\n", stu.age)
+	fmt.Printf("%d\n", stu.class)
+	fmt.Printf("%d\n", stu.score)
 }
