df5d51af20b84c14d11517b19738779aed9de475
wangkaixuan <2654189525@qq.com>
2019-02-16 23:55:16 +0800

    go语言学习: 学习了接口的定义和使用，主要的功能是为多个结构体实现相同的方法，可以通过同一个接口来调用不同的结构体方法

---
+++ 第09天-面向对象下/02接口的定义和使用.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Humaner interface {
+	SayHello()
+}
+type Student struct {
+	name string
+	age  int
+	sex  string
+}
+type Teacher struct {
+	name string
+	age  int
+	sex  string
+	sub  string
+}
+
+func (h *Teacheru) SayHello() {
+	fmt.Println(*h)
+}
+func (h *Student) SayHello() {
+	fmt.Println(*h)
+}
+
+func main() {
+	s := Student{"王凯旋", 24, "男"}
+	t := Teacher{"王凯旋", 24, "男", "计算机"}
+	var h Humaner
+	h = &s
+	h.SayHello()
+	h = &t
+	h.SayHello()
+}
