8d9dff4113d4657b3c42c1d7e34b0d2a21d97167
wangkaixuan <2654189525@qq.com>
2019-02-19 00:43:54 +0800

    go语言学习:做了两套题

---
+++ 第1套试卷-37.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	sex  byte
+	int
+}
+
+func main01() {
+	array := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s1 := array[:6:8]
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(s)
+}
+func sortScores(scores *[10]int) {
+	for i := 0; i < len(scores); i++ {
+		for j := i; j < len(scores); j++ {
+			if scores[i] < scores[j] {
+				scores[i], scores[j] = scores[j], scores[i]
+			}
+		}
+
+	}
+}
+func main() {
+	var scores [10]int = [10]int{80, 99, 39, 23, 91, 23, 56, 32, 56, 67}
+	sortScores(&scores)
+	fmt.Println(scores[:3])
+}
---
+++ 第1套试卷-38.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type employees struct {
+	id     string
+	name   string
+	age    int
+	salary int
+}
+
+func (e employees) print() {
+	fmt.Println("工号：%s", e.id)
+	fmt.Println("姓名：%s", e.name)
+	fmt.Println("年龄：%s", e.age)
+	fmt.Println("薪资：%s", e.salary)
+}
+func main() {
+	var e0 employees
+	var e1 employees
+	var e2 employees
+	fmt.Println("请输入第1位员工的姓名 年龄 薪资")
+	fmt.Scanf("%s %s %d %d", &e0.id, &e0.name, &e0.age, &e0.salary)
+	fmt.Println("请输入第2位员工的姓名 年龄 薪资")
+	fmt.Scanf("%s %s %d %d", &e1.id, &e1.name, &e1.age, &e1.salary)
+	fmt.Println("请输入第3位员工的姓名 年龄 薪资")
+	fmt.Scanf("%s %s %d %d", &e2.id, &e2.name, &e2.age, &e2.salary)
+	arr := []employees{e0, e1, e2}
+	for index := 0; index < len(arr); index++ {
+		if arr[index].salary > 8000 {
+			arr[index].print()
+		}
+	}
+}
