d6b34b769b19cb0078d814bdfa5a132f9af2e664
wangkaixuan <2654189525@qq.com>
2019-03-02 08:34:54 +0800

    go语言学习:做了两套题

---
+++ 第1套试卷-39.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Animal struct {
+	species string
+}
+
+type Speak interface {
+	call()
+}
+
+type Cat struct {
+	Animal
+}
+type Dog struct {
+	Animal
+}
+
+func (c *Dog) call() {
+	fmt.Println("汪汪叫")
+}
+func (c *Cat) call() {
+	fmt.Println("喵喵叫")
+}
+
+func Call(s Speak) {
+	s.call()
+}
+func main() {
+	d := Dog{Animal{"Dog"}}
+	c := Cat{Animal{"Cat"}}
+	Call(&d)
+	Call(&c)
+}
