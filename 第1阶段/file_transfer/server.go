11be611f3dc7c1034f8f39c5425b95bbaf42f682
wangkaixuan <2654189525@qq.com>
2019-03-09 03:16:52 +0800

    go项目-文件传输：完成了server的实现

--- 第1阶段/file_transfer/server.go
+++ 第1阶段/file_transfer/server.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
 
+func recvFile(fileName string, conn net.Conn) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("os.Create err", err)
+		return
+	}
+	defer f.Close()
+
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		fmt.Println(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("文件接受完毕")
+				return
+			} else {
+				fmt.Println("conn.Read err", err)
+				return
+			}
+			return
+		}
+		f.Write(buf[:n])
+	}
+
+}
+func main() {
+	// 创建tcp监听
+	lister, err := net.Listen("tcp", "127.0.0.1:8005")
+	if err != nil {
+		fmt.Println("net.Listen err", err)
+		return
+	}
+	defer lister.Close()
+
+	// 等待client连接
+	conn, err := lister.Accept()
+	if err != nil {
+		fmt.Println("net.Accept err", err)
+		return
+	}
+	defer conn.Close()
+
+	// 读取client发送的文件名
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("conn.Read err", err)
+		return
+	}
+
+	// 保存文件名
+	fileName := string(buf[:n])
+	conn.Write([]byte("ok"))
+	// 接受文件内容
+	recvFile(fileName, conn)
 }
