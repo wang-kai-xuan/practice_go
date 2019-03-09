f65a81d52be166d3d5ff40080f9e9d802011f621
wangkaixuan <2654189525@qq.com>
2019-03-09 10:47:01 +0800

    go语言学习: 实现了抓取了静态网页的程序

---
+++ 第1阶段/spider/demo1.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func HttpGet(url string) (res string, err error) {
+	resp, err1 := http.Get(url)
+	if err1 != nil {
+		err = err1
+		return
+	}
+	defer resp.Body.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, _ := resp.Body.Read(buf)
+		if n == 0 {
+			fmt.Println("读取完成")
+			break
+		}
+		res += string(buf[:n])
+	}
+	return
+}
+func working(start, end int) {
+	for i := start; i < end; i++ {
+		// 爬取数据
+		url := "https://tieba.baidu.com/f?kw=%E7%BB%9D%E5%9C%B0%E6%B1%82%E7%94%9F&ie=utf-8&pn=1" + strconv.Itoa((i-1)*50)
+		fmt.Println("正在爬取第", i, "页,url:", url)
+		res, err := HttpGet(url)
+
+		fileName := strconv.Itoa(i) + ".html"
+		f, err := os.Create(fileName)
+		if err != nil {
+			fmt.Println("Create err:", err)
+			continue
+		}
+		f.WriteString(res)
+		f.Close()
+	}
+}
+func main() {
+	//var start, end int
+	//fmt.Println("输入开始抓取的起始页：")
+	//fmt.Scan(&start)
+	//fmt.Println("输入结束抓取的终止页：")
+	//fmt.Scan(&end)
+	working(1, 3)
+}
