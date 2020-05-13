8f79a99308b0fb563801b546724bdaaff9eeaa69
wangkaixuan <2654189525@qq.com>
2020-05-13 13:05:19 +0800

    En: add TestLogrus

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/iroan/practice_go
 go 1.13
 
 require (
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
+	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
+	golang.org/x/sys v0.0.0-20200511232937-7e40ca221e25 // indirect
 	gopl.io v0.0.0-20191021194416-1e9cbf3a0aca
 )
--- log_test.go
+++ log_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	logrus "github.com/sirupsen/logrus"
 )
 
 // TestLogExample TestLogExample
@@ -17,4 +19,47 @@ func TestLogExample(t *testing.T) {
 
 	logger := log.New(logFile, "", log.Ltime|log.Lshortfile)
 	logger.Println("wkx test")
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.TextFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	name := "./" + time.Now().Format(time.RFC3339)[:10] + ".log"
+	logFile, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if nil != err {
+		panic(err)
+	}
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.TraceLevel)
+	logrus.SetOutput(logFile)
+
+}
+
+func TestLogrus(t *testing.T) {
+	logrus.WithFields(logrus.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	logrus.WithFields(logrus.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
+
+	// A common pattern is to re-use fields between logging statements by re-using
+	// the logrus.Entry returned from WithFields()
+	contextLogger := logrus.WithFields(logrus.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
+	logrus.WithFields(logrus.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Fatal("The ice breaks!")
 }
