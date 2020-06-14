1614c60329f220203cc183b0c752358b3d92c66c
wangkaixuan <2654189525@qq.com>
2020-06-15 00:25:33 +0800

    En: add TestSync

---
+++ sync_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestSync(t *testing.T) {
+	var once sync.Once
+	onceBody := func() {
+		fmt.Println("Only once")
+	}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onceBody)
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+}
