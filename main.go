d70a03504acd52c850bb1b518575e541b3e4bbf4
wangkaixuan <2654189525@qq.com>
2020-08-06 00:04:10 +0800

    En: parse html demo

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	parseAddr()
+}
+
+func parseAddr() {
+	buf, err := ioutil.ReadFile("./BitcoinFogAddresses.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(buf)
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" && strings.Contains(a.Val, "/address/") {
+				links = append(links, a.Val[9:])
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
+
+func testSignal() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
